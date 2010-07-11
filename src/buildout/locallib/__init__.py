e78555445dc09a7810450d1e0f76d90b14c87ac2
shimizukawa <devnull@localhost>
2010-07-11 19:31:49 +0900

    * add comments for class/function/method

--- src/buildout/locallib/__init__.py
+++ src/buildout/locallib/__init__.py
@@ -4,10 +4,14 @@ from distutils.sysconfig import get_python_lib
 SP_PATH = get_python_lib()
 
 class Installer(easy_install.Installer):
+    """ hook easy_install.Installer() for egg search path modification """
 
     base = None
 
     def __init__(self, *args, **kw):
+        # if exist args[5] (is path parameter) or 'path' keyword argument,
+        # update path list for including 'site-packages' path.
+
         #print '### easy_install.Installer hooking ...'
         if len(args) > 5:
             path = args[5]
@@ -20,6 +24,8 @@ class Installer(easy_install.Installer):
             path.append(SP_PATH)
 
         self.base.__init__(self, *args, **kw)
+
+    __init__.func_doc = easy_install.Installer.__init__.func_doc
 
 
 def load(buildout):
